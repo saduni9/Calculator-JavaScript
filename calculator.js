3679143533747b2e72f8f12bb5ffd1225727a129
saduni9 <rasikalahms.20@uom.lk>
2023-09-06 01:14:23 +0530

    Add new features

--- calculator.js
+++ calculator.js
@@ -3,15 +3,13 @@ const buttons = document.querySelectorAll("button");
 const specialChars = ["%","*","/","+","-","="];
 let output = "";
 
-
 const calculate = (btnValue) =>{
+    console.log("Input btnValue:", btnValue); // Debugging line
+
     if(btnValue === "=" && output !== ""){
-
         output = eval(output.replace("%","/100"));
-
     }else if(btnValue === "AC"){
         output ="";
-
     }else if(btnValue === "DEL"){
         output = output.toString().slice(0,-1);
     }else{
@@ -19,15 +17,17 @@ const calculate = (btnValue) =>{
         output += btnValue;
     }
 
+    console.log("Output:", output); // Debugging line
     display.value = output;
 };
 
 buttons.forEach(button => {
-    button.addEventListener("click",(e) => calculate(e.target.dataset.value));
+    button.addEventListener("click",(e) => {
+        console.log("Button clicked:", e.target.dataset.value); // Debugging line
+        calculate(e.target.dataset.value);
+    });
 });
-
 
 module.exports = {
     testEnvironment: 'jsdom',
-  };
-  
+};
